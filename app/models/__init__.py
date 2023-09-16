3648bf13e4af8915810bd9c3e3f1f3d948e1fdbf
Kourani <76729542m@gmail.com>
2023-09-16 08:54:03 -0400

    created product comment model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,5 @@
 from .db import db
 from .user import User
-from .business import Business 
+from .business import Business
+from .product_comment import ProductComment
 from .db import environment, SCHEMA
---
+++ app/models/product_comment.py
@@ -0,0 +1,24 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
+
+
+class ProductComment(db.Model):
+    __tablename__ = 'productComments'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    item_quality = db.Column(db.Integer)
+    comment = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, default = datetime.now())
+
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'itemQuality' : self.item_quality,
+            'comment' : self.comment,
+            'createdAt' : self.created_at
+        }
