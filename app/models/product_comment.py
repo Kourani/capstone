e983007c041b15febc4db6151a3d817253b46f42
Kourani <76729542m@gmail.com>
2023-09-16 19:50:06 -0400

    new seeders

--- app/models/product_comment.py
+++ app/models/product_comment.py
@@ -20,8 +20,8 @@ class ProductComment(db.Model):
 
     def to_dict(self):
         return {
-            'id': self.id,
-            'itemQuality' : self.item_quality,
-            'comment' : self.comment,
-            'createdAt' : self.created_at
+            'id':self.id,
+            'itemQuality':self.item_quality,
+            'comment':self.comment,
+            'createdAt':self.created_at
         }
