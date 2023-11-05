994d5d8d68816e4dda9f26bff31163d0d37a2309
Kourani <76729542m@gmail.com>
2023-11-05 17:44:42 -0500

    readme

--- react-app/src/store/cart.js
+++ react-app/src/store/cart.js
@@ -10,10 +10,19 @@ const addTo = (product) =>({
     type:ADD_TO,
     product
 })
+
+const deleteFrom = (id) =>({
+    type:REMOVE_FROM,
+    id
+})
 //--------------------------------------THUNKS--------------------------------------
 
 export const addCart = (payload) => async (dispatch) =>{
     dispatch(addTo(payload))
+}
+
+export const removeFrom = (itemId) => async (dispatch) =>{
+    dispatch(deleteFrom(itemId))
 }
 //------------------------------------REDUCER----------------------------------------------
 
